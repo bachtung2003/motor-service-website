1413862896a4604ca8c7c0bd6e253c29183e1927
tientrinh2003 <trinhthuytien1208@gmail.com>
2024-05-12 21:43:25 +0700

    CSS a little

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,80 +1,96 @@
 <template>
-  <div id="big-container" class="container-center">
-    <div class="container">
-      <div class="card">
-        <ProgressBar :value="progress[0]" class="custom-progress-bar"></ProgressBar>
-        <div class="topic">SELECT SERVICE</div>
-      </div>
-      <div class="card">
-        <ProgressBar :value="progress[1]" class="custom-progress-bar"></ProgressBar>
-        <div class="topic">CHOOSE AN AGENT</div>
-      </div>
-      <div class="card">
-        <ProgressBar :value="progress[2]" class="custom-progress-bar"></ProgressBar>
-        <div class="topic">PERSONAL INFORMATION</div>
-      </div>
-    </div>
+  <div>
+    <div id="big-container" class="container-center">
+      <div v-if="showConfirmation">
+        <div class="container">
+          <div class="card">
+            <ProgressBar :value="progress[0]" class="custom-progress-bar"></ProgressBar>
+            <div class="topic">SELECT SERVICE</div>
+          </div>
+          <div class="card">
+            <ProgressBar :value="progress[1]" class="custom-progress-bar"></ProgressBar>
+            <div class="topic">CHOOSE AN AGENT</div>
+          </div>
+          <div class="card">
+            <ProgressBar :value="progress[2]" class="custom-progress-bar"></ProgressBar>
+            <div class="topic">PERSONAL INFORMATION</div>
+          </div>
+        </div>
 
-    <div class="second-container">
-      <div v-if="step === 0">
-        <div class="message">Message</div>
-        <textarea class="input" v-model="userInfo.message"></textarea>
-        <div class="buttons">
-          <Button class="next-button" @click="nextStep">Next  ></Button>
-        </div>
-      </div>
-      <div v-else-if="step === 1">
-        <p>AGENT LIST</p>
-        <div class="buttons">
-          <Button class="back-button" @click="prevStep"><  Back</Button>
-          <Button class="next-button" @click="nextStep">Next  ></Button>
-        </div>
-      </div>
-      <div v-else-if="step === 2">
-        <div class="form-container">
-          <div class="input-group">
-            <label for="fullname">Name:</label>
-            <InputText id="fullname" v-model="userInfo.name" />
+        <div class="second-container">
+          <div v-if="step === 0">
+            <div class="message">Message</div>
+            <textarea class="input" v-model="userInfo.message"></textarea>
+            <div class="buttons">
+              <Button class="next-button" @click="nextStep">Next ></Button>
+            </div>
           </div>
-          <div class="input-group">
-            <label for="phone">Phone:</label>
-            <InputText id="phone" v-model="userInfo.phone" />
+          <div v-else-if="step === 1">
+            <p>AGENT LIST</p>
+            <div class="buttons">
+              <Button class="back-button" @click="prevStep">< Back</Button>
+              <Button class="next-button" @click="nextStep">Next ></Button>
+            </div>
           </div>
-          <div class="input-group">
-            <label for="email">E-mail:</label>
-            <InputText id="email" v-model="userInfo.email" />
+          <div v-else-if="step === 2">
+            <div class="form-container">
+              <div class="input-group">
+                <label for="fullname">Name:</label>
+                <InputText id="fullname" v-model="userInfo.name" />
+              </div>
+              <div class="input-group">
+                <label for="phone">Phone:</label>
+                <InputText id="phone" v-model="userInfo.phone" />
+              </div>
+              <div class="input-group">
+                <label for="email">E-mail:</label>
+                <InputText id="email" v-model="userInfo.email" />
+              </div>
+              <div class="input-group">
+                <label for="address">Address:</label>
+                <InputText id="address" v-model="userInfo.address" />
+              </div>
+              <div class="input-group">
+                <label for="dob">Appointment Date:</label>
+                <InputText id="dob" v-model="userInfo.dob" />
+              </div>
+            </div>
+            <div class="buttons">
+              <Button class="back-button" @click="prevStep">< Back</Button>
+              <Button class="submit-button" @click="submitForm">Submit</Button>
+            </div>
           </div>
-          <div class="input-group">
-            <label for="address">Address:</label>
-            <InputText id="address" v-model="userInfo.address" />
+          <div v-else>
+            <h1 class="content_header flex justify-content-center text-blue-700 text-5xl">
+              SUMMARY
+            </h1>
+            <p class="bottom-border">NAME: {{ userInfo.name }}</p>
+            <p class="bottom-border">PHONE: {{ userInfo.phone }}</p>
+            <p class="bottom-border">E-MAIL: {{ userInfo.email }}</p>
+            <p class="bottom-border">ADDRESS: {{ userInfo.address }}</p>
+            <p class="bottom-border">APPOINTMENT DATE: {{ userInfo.dateOfBirth }}</p>
+            <p>MESSAGE:</p>
+            <p class="sum-message">{{ userInfo.message }}</p>
+
+            <div class="summary">
+              <Button class="reset-button" @click="confirmStages">Confirm</Button>
+              <a class="reorder-link" @click="resetStages">Change the appointment</a>
+            </div>
           </div>
-          <div class="input-group">
-            <label for="dob">Appointment Date:</label>
-            <InputText id="dob" v-model="userInfo.dob" />
-          </div>
-        </div>
-        <div class="buttons">
-          <Button class="back-button" @click="prevStep"><  Back</Button>
-          <Button class="submit-button" @click="submitForm">Submit</Button>
         </div>
       </div>
       <div v-else>
-        <h1 class="content_header flex justify-content-center text-blue-700 text-5xl">SUMMARY</h1>
-        <p class="bottom-border">NAME: {{ userInfo.name }}</p>
-        <p class="bottom-border">PHONE: {{ userInfo.phone }}</p>
-        <p class="bottom-border">E-MAIL: {{ userInfo.email }}</p>
-        <p class="bottom-border" >ADDRESS: {{ userInfo.address }}</p>
-        <p class="bottom-border" >APPOINTMENT DATE: {{ userInfo.dateOfBirth }}</p>
-        <p>MESSAGE: </p>
-        <p class="sum-message">{{ userInfo.message }}</p>
-     
+        <div class="second-container">
+          <h1 class="content_header flex justify-co.1051000200 ntent-center text-grey-700 text-2xl">
+            THANK YOU FOR YOUR REQUEST, WE HAVE SENT THE CONFIRMATION LETTER TO YOUR EMAIL.
+          </h1>
+        </div>
         <div class="summary">
-          <Button class="reset-button" @click="confirmStages">Confirm</Button>
-          <a class="reorder-link" @click="resetStages">Change the appointment</a>
+          <Button class="reset-button">Return</Button>
         </div>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -98,8 +114,9 @@ export default {
         address: '',
         email: '',
         phone: '',
-        message:''
-      }
+        message: ''
+      },
+      showConfirmation: true
     }
   },
   methods: {
@@ -118,6 +135,9 @@ export default {
     resetStages() {
       this.step = 0
       this.progress = [0, 0, 0]
+    },
+    confirmStages() {
+      this.showConfirmation = false
     }
   }
 }
@@ -125,11 +145,11 @@ export default {
 
 <style scoped>
 * {
-  color: #303F9F;
+  color: #303f9f;
 }
 
 #big-container {
-  width: 902px;
+  width: 1200px;
   height: 600px;
   display: flex;
   padding-top: 50px;
@@ -210,6 +230,7 @@ export default {
   color: white;
 }
 </style>
+
 <style>
 .p-progressbar-value {
   background-color: black;
@@ -217,9 +238,10 @@ export default {
 .p-button {
   justify-content: center;
   font-size: 16px;
+  font-weight: 600;
   color: #fe7a36;
   background-color: white;
-  border: 2px solid #fe7a36;
+  border: 1px solid #fe7a36;
   border-radius: 0px;
   padding: 10px 20px;
   cursor: pointer;
@@ -251,46 +273,52 @@ export default {
 }
 
 .second-container > div:not(.card) {
-  background-color: #fef9f7;
   padding: 20px;
   border-radius: 5px;
   margin-top: 20px;
 }
 
 .second-container > div:not(.card) p {
-  margin-bottom: 28px;
-  padding-bottom: 10px;
   font-size: 16px;
   color: #545454;
   font-weight: 600;
+  padding-left: 20px;
+  padding-bottom: 20px;
 }
 
-.bottom-border{
+.bottom-border {
   border-bottom: 1px solid #e2d9d9;
+  margin-bottom: 30px;
 }
 
 .text-5xl {
-  font-size: 2rem !important;
-  font-weight: 650;
+  font-size: 3rem !important;
+  font-weight: 700;
 }
 
-.summary{
+.summary {
   display: flex;
   flex-direction: column;
-  padding-top: 20%;
+  padding-top: 10%;
   align-items: center;
 }
 
-.sum-message{
-  background-color: #D9D9D9;
-  padding:20px;
-  margin-left:5%;
-  margin-right:5%;
+.sum-message {
+  background-color: #d9d9d9;
+  padding: 20px;
+  margin-left: 30px;
+  margin-right: 30px;
 }
 
 .reorder-link {
   color: grey;
   text-decoration: underline;
   cursor: pointer;
+}
+
+.text-2xl {
+  font-size: 1.5rem !important;
+  color: #545454;
+  font-weight: 600;
 }
 </style>
